1ac1ce6c7a0731141c947b41f592b1b2b76bd027
Govind <govindivya801@gmail.com>
2022-03-15 18:46:41 +0530

    some changes

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Loading = () => {
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((time) => time + 100);
+    });
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div className="loding">
-        <div className="spin"></div>
+      {time < 10000 ? (
+        <p>We are authenticating you. Please wait !</p>
+      ) : (
+        <p>It is taking more time than usual.</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
